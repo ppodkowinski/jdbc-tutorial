e37a91d7c165e5025628c404ad8527243801ba8f
unknown <przemyslaw.podkowinski@stazysta.comarch.pl>
2015-07-09 11:53:00 +0200

    Add new spring function based jdbc

--- src/main/java/com/acme/order/JdbcOrderRepository.java
+++ src/main/java/com/acme/order/JdbcOrderRepository.java
@@ -12,9 +12,13 @@ import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import com.acme.order.delivery.strategy.PizzaTypeDeliveryTimeStrategy;
 import com.acme.order.pizza.PizzaOrder;
+import com.acme.order.pizza.PizzaType;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import lombok.extern.log4j.Log4j;
@@ -30,50 +34,53 @@ public class JdbcOrderRepository implements OrderRepository {
 	private final String user = "dbuser";
 
 	private final String password = "dbpass";
-	
+
 	@Autowired
 	private DataSource dataSource;
 	
+	  private JdbcTemplate jdbcTemplate;
+
+	    public void setDataSource(DataSource dataSource) {
+	        this.jdbcTemplate = new JdbcTemplate(dataSource);
+	    }
 
 	@Override
-	public String save(PizzaOrder order) {		
-				
+	public String save(PizzaOrder order) {
+
 		return null;
 	}
 
 	@Override
 	public void rollback() {
-		
-		
+
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public PizzaOrder get(String pizzaOrderId) {
-		try(Connection conn = dataSource.getConnection()){
-			try(Statement stmt = conn.createStatement();
-				ResultSet rs= stmt.executeQuery("Select * from order_t where id = 1"))
-				{
-				System.out.println(rs.toString());
-			}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		return null;
+		return this.jdbcTemplate.queryForObject("select * from order_t where id=?", new Object[]{pizzaOrderId}, PizzaOrder.class);
 	}
 
 	@Override
 	public List<PizzaOrder> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.jdbcTemplate.queryForList("select * from order_t",PizzaOrder.class);
 	}
 
 	@Override
 	public List<PizzaOrder> findByOrderStatus(OrderStatus orderStatus) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.jdbcTemplate.query("select * from order_t where status= ?",new Object[]{orderStatus},new RowMapper<PizzaOrder>(){
+
+			@Override
+			public PizzaOrder mapRow(ResultSet rs, int arg1) throws SQLException {
+				rs.getString("type");
+				Customer customer = new Customer("1", "PPP", "pppd@sjdas", "Lodz");
+				PizzaType pizzatype = PizzaType.BIG;
+				PizzaOrder po = new PizzaOrder(customer, pizzatype);
+				return po;
+			}
+			
+		});
 	}
 
 }
